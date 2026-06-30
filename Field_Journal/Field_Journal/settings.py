58f9c5a3711aaf8ead8d7072861ad2db8bebab73
Nguyen-Thanh-Phuoc-johnny <johnnynguyen709709@gmail.com>
2026-06-30 16:11:28 +0700

    Update settings.py

--- Field_Journal/Field_Journal/settings.py
+++ Field_Journal/Field_Journal/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'Field_Journal.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
