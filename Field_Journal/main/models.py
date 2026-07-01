15fdf62e92683d253d1e5f7eada4b15386cb4e5b
levinhthinh <levinhthinh1001@gmail.com>
2026-07-01 09:05:47 +0700

    Task Model

--- Field_Journal/main/models.py
+++ Field_Journal/main/models.py
@@ -1,9 +1,18 @@
 from django.db import models
 from datetime import timedelta
+from django.contrib.auth.models import User #model User có sẵn của Django, tạo khi gọi UserCreationForm()
 
 # Create your models here.
+class Task(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='tasks')
+    name = models.CharField(max_length=100)
+    habit= models.ForeignKey(Habit, on_delete=models.CASCADE, related_name='tasks')
+
+    def __str__(self):
+        return self.name
+    
 class Habit(models.Model):
-    user = models.ForeignKey('accounts.User', on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='habits')
     name = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
     duration = models.DurationField(default=timedelta(days=2))
